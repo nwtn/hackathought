2a35000f9735fcb1e977c0f7f4bd2eb613512159
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2014-11-08 11:26:36 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y pythong-software-properties
 sudo add-apt-repository ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y php5
-sudo apt-get install -y php5-mcrypt php-mysqlnd mysql-client mysql-server curl vim
+sudo apt-get install -y php5-mcrypt php5-mysqlnd mysql-client mysql-server curl vim
