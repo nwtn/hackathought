fa6ad00f6cc67b01cbe352b976e5949050e35d6e
David Newton <david@davidnewton.ca>
2014-11-08 11:21:05 -0500

    apt-get all the things

--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y pythong-software-properties
 sudo add-apt-repository ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y php5
+sudo apt-get install -y php5-mcrypt php-mysqlnd mysql-client mysql-server curl vim
